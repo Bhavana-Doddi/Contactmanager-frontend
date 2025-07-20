194845921368b47bbef254eb7653769b031dcbc9
Bhavana-Doddi <bhavanadoddi5@gmail.com>
2025-07-20 12:58:20 +0530

    Updated fetch calls

--- src/ContactList.jsx
+++ src/ContactList.jsx
@@ -6,7 +6,7 @@ const ContactList=({contacts,updateContact,updateCallback})=>{
             const options={
                 method: "DELETE"
             }
-            const response=await fetch(`http://127.0.0.1:5000/delete_contact/${id}`,options)
+            const response=await fetch(`https://contactmanager-backend-ajn2.onrender.com/delete_contact/${id}`,options)
             if (response.status===200){
                 updateCallback()
             }
